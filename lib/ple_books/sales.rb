8183defcb6ace151ba7e839f2be7b47e65a4cb3c
Cesar Carruitero <cesar@mozilla.pe>
2017-01-10 12:22:11 -0500

    fix typo

---
+++ lib/ple_books/sales.rb
@@ -0,0 +1,14 @@
+require 'ple_books/base'
+
+module PleBooks
+  class Sales < Base
+    def initialize(ruc, tickets, month, year)
+      book_name = self.class.name.downcase.sub("plebooks::", "")
+      dir = File.dirname(__FILE__)
+      fields = YAML.load_file("#{dir}/layouts/#{book_name}.yml")
+
+      @filename = "#{path}#{ple_book_name('14.2', ruc, month, year)}.txt"
+      get_file(tickets, fields, @filename)
+    end
+  end
+end
