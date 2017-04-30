feabfe2b3d66a26e83263818c37fe648517eed99
Cesar Carruitero <cesar@mozilla.pe>
2017-04-30 14:36:32 -0500

    some test for pdf

---
+++ test/books/buys_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require_relative "../helper"
+
+setup do
+  @company = Company.new(ruc: Faker::Number.number(11), name: Faker::Name.name)
+end
+
+test "render pdf, have a parseable pdf" do
+  tickets = []
+  view = Object.new.tap { |o| o.extend(Prawn::View) }
+  pdf = Books::Buys.new(@company, tickets, view, 2, 3015)
+  page_counter = PDF::Inspector::Page.analyze(pdf.render)
+  assert pdf.page_count == 1
+  assert page_counter.pages.size == 1
+end
+
+test "have correct text in header" do
+  tickets = []
+  view = Object.new.tap { |o| o.extend(Prawn::View) }
+  pdf = Books::Buys.new(@company, tickets, view, 2, 3015)
+  # pdf_analyzed = PDF::Inspector::Text.analyze(pdf.render)
+  # puts pdf_analyzed.strings
+  reader = PDF::Reader.new(StringIO.new(pdf.render))
+  assert reader.pages.first.text.include?("REGISTRO DE COMPRAS")
+end
--- test/helper.rb
+++ test/helper.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 require "cutest"
 require "pry"
+require "faker"
 require_relative "../lib/sunat_books"
 require_relative "fixtures/ticket"
 require_relative "fixtures/company"
+
+Prawn.debug = true
+
+require "pdf/inspector"
 
 def get_line(array, object)
   str = ""
