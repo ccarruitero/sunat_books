6b717fb9f2aa4867afe5aa5c1394097c2c1eeff8
Cesar Carruitero <cesar@mozilla.pe>
2017-05-31 02:12:26 -0500

    fixing cobs for simplify diary

---
+++ lib/books/diary_entries.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+module DiaryEntries
+  def initial_entry(tickets, counts, total_sums)
+    initial = tickets.where(operation_type: "inicial")
+    if !initial.empty?
+      initial_data = get_row_sums(initial, counts, total_sums)
+    else
+      initial_data = []
+      total_sums.map do |sum|
+        initial_data << { content: formated_number(sum.total), align: :right }
+      end
+    end
+    initial_data
+  end
+
+  def sales_entry(tickets, counts, total_sums)
+    sales = tickets.where(operation_type: "ventas")
+    # if sales.count.positive?
+    #   sales entry
+    sales_sum = get_row_sums(sales, counts, total_sums)
+    # end
+    sales_sum
+  end
+
+  def other_entry(tickets, counts, total_sums, data)
+    # other entries
+    others = tickets.where(operation_type: "otros")
+    # others_row = get_row_sums(others, counts, total_sums)
+    others&.each do |ticket|
+      ticket_data = []
+      counts.each_with_index do |count, i|
+        value = mother_count?(count, ticket) ? get_value(ticket, count) : 0
+        increase_value(ticket_data, total_sums, i, value)
+      end
+      ref = ticket.reference
+      data << [parse_day(ticket.operation_date), ref, ticket_data].flatten
+    end
+  end
+
+  def close_entry(tickets, counts, total_sums, data)
+    close = tickets.where(operation_type: "cierre")
+    close.each do |ticket|
+      ticket_data = []
+      counts.each_with_index do |count, i|
+        value = mother_count?(count, ticket) ? get_value(ticket, count) : 0
+        increase_value(ticket_data, total_sums, i, value)
+      end
+      ref = ticket.reference
+      data << [parse_day(ticket.operation_date), ref, ticket_data].flatten
+    end
+  end
+
+  def total_entry(total_sums, data)
+    # totals
+    total_data = []
+    total_sums.map do |sum|
+      total_data << { content: formated_number(sum.total), align: :right }
+    end
+    data << [{ content: "TOTALES", colspan: 2 }, total_data].flatten
+  end
+
+  def buys_entry(tickets, counts, total_sums, data, period_date)
+    buys = tickets.where(operation_type: "compras")
+    title = "COMPRAS DEL PERIODO"
+    if buys.count.positive?
+      # buys entry
+      buys_row = get_row_sums(buys, counts, total_sums)
+      data << [period_date, title, buys_row].flatten
+    end
+  end
+
+  def mother_count?(count, ticket)
+    ticket.uniq_mother_counts.include?(count)
+  end
+
+  def calculate_totals(tickets, count_sums)
+    # get totals
+    tickets.each do |ticket|
+      count_sums.each do |count_sum|
+        count_sum.add get_value(ticket, count_sum.count)
+      end
+    end
+  end
+
+  def current_sum_count(count_sums, count)
+    sum_count = nil
+    count_sums.each do |count_sum|
+      sum_count = count_sum if count_sum.count == count
+    end
+    sum_count
+  end
+
+  def get_row_sums(tickets, counts, total_sums)
+    # given an array of counts and tickets get sums by each count
+    row_counts = get_mother_counts tickets
+    count_sums = row_counts.map { |count| Books::CountSum.new(count) }
+
+    calculate_totals(tickets, count_sums)
+    # get ordered row
+    row_data = []
+    counts.each_with_index do |count, i|
+      sum_count = current_sum_count(count_sums, count)
+      value = sum_count ? sum_count.total : 0
+      increase_value(row_data, total_sums, i, value)
+    end
+    row_data
+  end
+
+  def increase_value(row_data, total_sums, i, value)
+    total_sums[i].add value
+    row_data << { content: formated_number(value), align: :right }
+  end
+end
--- lib/books/simplified_diary.rb
+++ lib/books/simplified_diary.rb
@@ -2,9 +2,11 @@
 
 require_relative "base"
 require_relative "count_sum"
+require_relative "diary_entries"
 
 module Books
   class SimplifiedDiary < Base
+    include DiaryEntries
     def initialize(company, tickets, view, month, year)
       super(page_layout: :landscape, margin: [5], page_size: "A4")
       @view = view
@@ -13,100 +15,63 @@ module Books
       # @book_name = self.class.name.downcase.sub("books::", "")
       # dir = File.dirname(__FILE__)
       # @blayout = YAML.load_file("#{dir}/layouts/#{@book_name}.yml")
-      counts = get_mother_counts @tickets
-      total_sums = counts.map { |count| CountSum.new(count) }
 
+      prawn_book(month, year)
+    end
+
+    def prawn_book(month, year)
       (month.to_i..12).each do |m|
         start_new_page unless m == month.to_i
         period = get_period(m, year)
 
-        bounding_box([bounds.left + 3, bounds.top - 10], width: 815, height: 510) do
-          book_body m, year, total_sums, 20, period
+        x = bounds.left + 3
+        y = bounds.top - 10
+        bounding_box([x, y], width: 815, height: 510) do
+          book_body m, year, 20, period
         end
       end
     end
 
-    def book_body(month, year, total_sums, max_column = nil, period = nil)
-      data = []
+    def book_body(month, year, max_column = nil, period = nil)
       tickets = @tickets.where(period_month: month, period_year: year)
+      @main_title = "LIBRO DIARIO - FORMATO SIMPLIFICADO"
+      counts = get_mother_counts @tickets
+      total_sums = counts.map { |count| CountSum.new(count) }
 
       # header
-      # counts = get_counts @tickets
-      counts = get_mother_counts @tickets
+      data = []
       data << ["FECHA", "OPERACIÓN", counts].flatten
 
       # body
-
-      # initial entry
-      initial = tickets.where(operation_type: "inicial")
-      if !initial.empty?
-        initial_data = get_row_sums(initial, counts, total_sums)
-      else
-        initial_data = []
-        total_sums.map do |sum|
-          initial_data << { content: formated_number(sum.total), align: :right }
-        end
-      end
+      initial_data = initial_entry(tickets, counts, total_sums)
       date = get_date(year.to_i, month.to_i, 1)
       data << [date, "ASIENTO INICIAL DEL PERIODO", initial_data].flatten
 
       if !tickets.empty?
-        sales = tickets.where(operation_type: "ventas")
-        if sales.count.positive?
-          # sales entry
-          sales_sum = get_row_sums(sales, counts, total_sums)
-          sales_row = [get_date(year.to_i, month.to_i, -1), "VENTAS DEL PERIODO", sales_sum].flatten
-          data << sales_row
-        end
-
-        buys = tickets.where(operation_type: "compras")
-        if buys.count.positive?
-          # buys entry
-          buys_row = get_row_sums(buys, counts, total_sums)
-          data << [get_date(year.to_i, month.to_i, -1), "COMPRAS DEL PERIODO", buys_row].flatten
-        end
-
-        # other entries
-        others = tickets.where(operation_type: "otros")
-        # others_row = get_row_sums(others, counts, total_sums)
-        others.each do |ticket|
-          ticket_data = []
-          counts.each_with_index do |count, i|
-            if ticket.uniq_mother_counts.include? count
-              value = get_value(ticket, count)
-            else
-              value = 0
-            end
-            total_sums[i].add value
-            ticket_data << { content: formated_number(value), align: :right }
-          end
-          data << [parse_day(ticket.operation_date), ticket.reference, ticket_data].flatten
-        end
-
-        # cierre entry
-        close = tickets.where(operation_type: "cierre")
-        close.each do |ticket|
-          ticket_data = []
-          counts.each_with_index do |count, i|
-            value = ticket.uniq_mother_counts.include?(count) ? get_value(ticket, count) : 0
-            total_sums[i].add value
-            ticket_data << { content: formated_number(value), align: :right }
-          end
-          data << [parse_day(ticket.operation_date), ticket.reference, ticket_data].flatten
-        end
+        period_date = get_date(year, month, -1)
+        entries_data(tickets, counts, total_sums, data, period_date)
       else
         data << [{ content: "SIN MOVIMIENTO EN EL PERIODO", colspan: 5 }]
       end
 
-      # totals
-      total_data = []
-      total_sums.map do |sum|
-        total_data << { content: formated_number(sum.total), align: :right }
-      end
-      data << [{ content: "TOTALES", colspan: 2 }, total_data].flatten
+      total_entry(total_sums, data)
+      book_header period, @company.ruc, @company.name, @main_title
 
-      book_header period, @company.ruc, @company.name, "LIBRO DIARIO - FORMATO SIMPLIFICADO"
+      draw_table(data, max_column, period)
+    end
 
+    def entries_data(tickets, counts, total_sums, data, period_date)
+      sales_sum = sales_entry(tickets, counts, total_sums)
+      title = "VENTAS DEL PERIODO"
+      sales_row = [period_date, title, sales_sum].flatten
+      data << sales_row
+
+      buys_entry(tickets, counts, total_sums, data, period_date)
+      other_entry(tickets, counts, total_sums, data)
+      close_entry(tickets, counts, total_sums, data)
+    end
+
+    def draw_table(data, max_column, period)
       if data.first.count > max_column
         tmp0 = []
         tmp1 = []
@@ -114,7 +79,7 @@ module Books
         data.each do |column|
           if column == data.last
             first_page = column.first(max_column - 1)
-            second_page = (column[(max_column - 1)..column.length])
+            second_page = column[(max_column - 1)..column.length]
             tmp0 << first_page
             next_page = [column.first] + second_page
             tmp1 << next_page
@@ -130,18 +95,19 @@ module Books
           end
         end
 
-        table(tmp0, header: true, cell_style: { borders: [], size: 6 },
-                    column_widths: { 1 => 73 })
+        render_prawn_table(tmp0)
         start_new_page
-        book_header period, @company.ruc, @company.name, "LIBRO DIARIO - FORMATO SIMPLIFICADO"
-        table(tmp1, header: true, cell_style: { borders: [], size: 6 },
-                    column_widths: { 1 => 73 })
+        book_header period, @company.ruc, @company.name, @main_title
+        render_prawn_table(tmp1)
 
       else
-
-        table(data, header: true, cell_style: { borders: [], size: 6 },
-                    column_widths: { 1 => 73 })
+        render_prawn_table(data)
       end
+    end
+
+    def render_prawn_table(data)
+      table(data, header: true, cell_style: { borders: [], size: 6 },
+                  column_widths: { 1 => 73 })
     end
   end
 end
