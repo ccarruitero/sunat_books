cb521cdf4bfa781100943d56c3221201caf4045d
César Carruitero <ccarruitero@protonmail.com>
2020-05-30 23:02:45 -0500

    docs: add documentation comments for Ple::Sales methods

--- lib/sunat_books/ple/sales.rb
+++ lib/sunat_books/ple/sales.rb
@@ -5,6 +5,14 @@ require "sunat_books/ple/base"
 module SunatBooks
   module Ple
     class Sales < Base
+      # ruc => company's ruc in string format
+      # tickets => an array of objects that respond to a layout's methods
+      # month => a number that represent a month
+      # year => a number that represent a year
+      # options =>
+      #   :yml  => to define a custom layout file
+      #   :layout => to define a custom name for a specific layout method
+      #   :book_format => to define ple book format to use
       def initialize(ruc, tickets, month, year, options = {})
         @book_format = options[:book_format] || "14.2"
         yml_path = options[:yml] || default_yml(book_format)
@@ -19,6 +27,8 @@ module SunatBooks
 
       private
 
+      # get default yml file according book format given
+      # book_format => ple book format
       def default_yml(book_format)
         book_name = self.class.name.downcase.sub("sunatbooks::ple::", "")
         dir = File.dirname(__FILE__)
